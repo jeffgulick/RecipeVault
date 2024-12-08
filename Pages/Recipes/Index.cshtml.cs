db557222de44485e2afd80dd0594ef13e18053a9
JeffGulick <jeffgulick@gmail.com>
2024-12-08 13:34:54 -0600

    working on search

--- Pages/Recipes/Index.cshtml.cs
+++ Pages/Recipes/Index.cshtml.cs
@@ -14,6 +14,9 @@ namespace RecipeVault.Pages_Recipes
         public int PageSize {get; set;} = 5;
         public int TotalPages {get; set;}
 
+        [BindProperty(SupportsGet = true)]
+        public string CurrentSearch { get; set; } = string.Empty;
+
         public IndexModel(RecipeVault.Models.AppDbContext context)
         {
             _context = context;
@@ -23,13 +26,21 @@ namespace RecipeVault.Pages_Recipes
 
         public async Task OnGetAsync()
         {
-            // Recipe = await _context.Recipes
-            //     .Include(r => r.Category).ToListAsync();
+            var query = _context.Recipes.Include(i => i.Category).AsQueryable();
 
+            // Search
+            if (!string.IsNullOrEmpty(CurrentSearch))
+            {
+                query = query.Where(r => r.RecipeName.Contains(CurrentSearch) ||
+                                     r.Category.CategoryName.Contains(CurrentSearch));
+            }
+            
             TotalPages = (int)Math.Ceiling(_context.Recipes.Count() / (double)PageSize);
     
-            Recipe = await _context.Recipes.Include(r => r.Category!)
+            Recipe = await query
                 .Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync(); 
         }
+
+    
     }
 }
